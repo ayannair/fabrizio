69068d87ebc4fb83b272817e8384b41ce69df557
Ayan Nair <ayannair23@gmail.com>
2025-06-01 16:01:06 -0700

    removed a line

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
       const data = await res.json();
       const keywords = data.keywords;
       setKeywords(keywords);
-      console.log(keywords)
     };
     fetchKeywords();
   }, []);
